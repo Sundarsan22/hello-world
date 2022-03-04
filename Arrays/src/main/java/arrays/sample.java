f656eba7779d49164f6be2e5e79ee8af16d9ed5f
Sundarsan22 <90100142+Sundarsan22@users.noreply.github.com>
2022-03-04 15:54:29 +0530

    Add files via upload

---
+++ Arrays/src/main/java/arrays/sample.java
@@ -0,0 +1,11 @@
+package arrays;
+
+public class sample {
+	public static void main(String[] args) {
+		
+	int num1=5, num2=3,sum;
+	sum = num1+num2;
+	System.out.println("sum of the numbers:" + sum);
+
+}
+}
